206717663107d31a9e085eb17af1d7f19c400499
Daniel Ortiz <DOrtiz1011@gmail.com>
2013-02-14 10:24:25 -0500

    Initial check in of blank Android project

---
+++ src/edu/ncc/checkers/CheckersActivity.java
@@ -0,0 +1,22 @@
+package edu.ncc.checkers;
+
+import android.os.Bundle;
+import android.app.Activity;
+import android.view.Menu;
+
+public class CheckersActivity extends Activity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.checkers_activity);
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        // Inflate the menu; this adds items to the action bar if it is present.
+        getMenuInflater().inflate(R.menu.checkers_activity, menu);
+        return true;
+    }
+    
+}
